c60a3386bebb1d0b897b6276927eaa1e025af139
VincentEmmanuelS <suwardyvincent@gmail.com>
2025-06-04 17:32:52 +0700

    feat: client connected to server

--- Server/Server.go
+++ Server/Server.go
@@ -1,5 +1,26 @@
 package main
 
-func main() {
+import (
+	"fmt"
+	"net"
+)
 
+func main() {
+	listener, err := net.Listen("tcp", ":2000")
+	if err != nil {
+		fmt.Println("Error starting server:", err)
+		return
+	}
+	defer listener.Close()
+	fmt.Println("Server started. Waiting for clients...")
+
+	for {
+		conn, err := listener.Accept()
+		if err != nil {
+			fmt.Println("Error accepting connection:", err)
+			continue
+		}
+		fmt.Printf("Client connected: %s\n", conn.RemoteAddr().String())
+		conn.Close() // langsung tutup koneksi, hanya untuk cek koneksi saja
+	}
 }
